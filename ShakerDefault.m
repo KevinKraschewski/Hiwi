8a2e70efe01b3ddad0ef81d9af0fb411802c1dc0
KevinKraschewski <kevin.kraschewski@web.de>
2015-10-04 19:01:59 +0200

    getDiscr method added
    Neue Methode, die allerdings noch nicht auf Funktionalitaet getestet
    wurde! Außerdem neue Attribute hinzugefuegt und entspr. Optionen im
    Konstruktor

--- ShakerDefault.m
+++ ShakerDefault.m
@@ -1,19 +1,31 @@
 classdef ShakerDefault < models.muscle.AMuscleConfig
     
+    
     properties(SetAccess=private)
         ylen;
         radfun;
         stretchfun;
+        %New Parameters for frequency, a tolerance for the maxDy of
+        %TMRFunction (for the discretisation part) and the maximal amount
+        %of points that are usable
         frequency;
+        TOL;
+        maxYPoints; %Default als 0! Fuer die Disc Methode (Abfrage ob der Wert 0 ist)
     end
     
     methods
         function this = ShakerDefault(varargin)
             this = this@models.muscle.AMuscleConfig(varargin{:});
+           
+            
             %Default Stretch is exp
             this.addOption('Stretch','exp');
             %Default frequency is 50Hz
             this.addOption('Frequency',50);
+            %Default tolerance 0.1
+            this.addOption('TOL',10^(-1));
+            %Default maxYPoints 0, as this value is used in a method!
+            this.addOption('maxYPoints',0);
             
             
             
@@ -27,6 +39,10 @@ classdef ShakerDefault < models.muscle.AMuscleConfig
             
             %Sets the frequency property
             this.frequency=this.Options.Frequency;
+            %Sets the tolerance property
+            this.TOL=this.Options.TOL;
+            %Sets the maxYPoints property
+            this.maxYPoints=this.Options.maxYPoints;
             
             ref = [1.03, 1.4];
             %Man kann nun ueber 'Gauss Gamma' einen GaußKernel als
@@ -117,13 +133,84 @@ classdef ShakerDefault < models.muscle.AMuscleConfig
             % r = (F/A - }\sigma_m) / (sigma_t - sigma_m)
             tmr = (F./A(ycoord) - musclestress)./(tendonstress-musclestress);
         end
+        
+        function [ypoints] = getDiscr(this,TMRFunc)
+            deltaX=this.ylen/4; %Anfangsschrittweite           
+            maxdy=this.TOL; %Default maximales dy als Differenzen der TMRFunktionswerte
+            
+            
+            if this.maxYPoints~=0 %Anzahl an Intervallpunkten vorgegeben
+                ypoints=zeros(this.maxYPoints,1); %Initialisieren
+                ypoints(this.maxYPoints)=this.ylen; %Letzten Punkt fest
+                %%Schleife ueber die vorgegebene Anzahl an Punkten
+                %%abzueglich dem letzten Punkt, da dieser Fest ist
+                for i=1 : this.maxYPoints-1 %Erster und letzter Punkt fest
+                    
+                    %Solange man mit dem naechsten Punkt rechts rausfallen
+                    %wuerde
+                    while ypoints(i)+deltaX > this.ylen
+                        deltaX=deltaX/2;
+                    end
+                    
+                    %%Schleife bis Tolerierte Schrittweite erreicht wird
+                    while abs(TMRFunc(ypoints(i))-TMRFunc(ypoints(i)+deltaX))>this.TOL; %Solange die tolerierte Schrittweite nicht erreicht wird repeat
+                        if abs(TMRFunc(ypoints(i))-TMRFunc(ypoints(i)+deltaX))<this.TOL;%Schrittweite akzeptiert
+                            ypoints(i+1)=ypoints(i)+deltaX;                        %Naechster Punkt ist der alte plus ermitteltes "notwendiges" deltaT 
+                            maxdy=abs(TMRFunc(ypoints(i))-TMRFunc(ypoints(i)+deltaX));
+                        else %Schrittweite nicht akzeptiert -> Versuche es mit halber Schrittweite
+                       deltaX=deltaX/2;
+                        end                        
+                    end
+                    deltaX=this.ylen/4; %Schrittweite am Ende immer auf Anfangsschrittweite zuruecksetzen
+                end
+                    %Letzte Abfrage ob insgesamt die Toleranz erreicht
+                    %wurde
+                    if abs(TMRFunc(this.ylen)-TMRFunc(ypoints(this.ylen-1)))>this.TOL; %Alle anderen Punkte erfuellen die Toleranzbedingung -> Letzter Test
+                        maxdy=abs(TMRFunc(this.ylen)-TMRFunc(ypoints(this.ylen-1)));
+                        error(['The Tolerance can not be reached with this amount of points. The max of dy would be ' num2str(maxdy)]);
+                    
+                    else
+                        disp(['The Tolerance was reached with the number of points and the max of dy is' num2str(maxdy)]);
+                    end
+                
+            else %Nur Toleranz vorgegeben -> So viele Punkte wie "noetig".
+                ypoints=zeros(1000,1); %Preallocation for better performance
+                i=1;
+                
+                while 1 %Endless loop until tolerance globally reached
+                    
+                    %Solange man mit dem naechsten Punkt rechts rausfallen
+                    %wuerde
+                    while ypoints(i)+deltaX > this.ylen
+                        deltaX=deltaX/2;
+                    end                    
+                    
+                    if (abs(TMRFunc(ypoints(i))-TMRFunc(this.ylen))) < this.TOL %Toleranz insgesamt erreicht -> Break
+                        ypoints(i+1)=this.ylen; %Letzter Punkt fix
+                        ypoints=ypoints(1:i); %Nur die wirklich gebrauchten Punkte verwenden, restl 0en der Preallocation "wegschneiden"
+                        disp(['Tolerance reached and used' num2str(i) 'points, maxDy is' num2str(maxdy)]);
+                        break
+                    else
+                        while abs(TMRFunc(ypoints(i))-TMRFunc(ypoints(i)+deltaX)) > this.TOL
+                        deltaX=deltaX/2;
+                        end
+                        maxdy=TMRFunc(ypoints(i))-TMRFunc(ypoints(i)+deltaX);
+                        ypoints(i+1)=ypoints(i)+deltaX;
+                    end
+                    i=i+1;
+                end
+            end
+        end
     end
     
     methods(Access=protected)
         
         function geo = getGeometry(this)
-            geo = fem.geometry.Belly(linspace(0,this.ylen,15),...
-                'Radius',this.radfun,'InnerRadius',0);
+%             f=
+%             tmrFunc=@(y) (F./A(y)-f.AnisoPassiveMuscle(this.stretchfun(y)))./(f.AnisoPassiveTendon(this.stretchfun(y))-f.AnisoPassiveMuscle(this.stretchfun(y)));
+%             [ypoints] = getDiscr(this,1/x);
+            ypoints=linspace(0,this.ylen,15);
+            geo= fem.geometry.Belly(ypoints,'Radius',this.radfun,'InnerRadius',0);
         end
         
         function displ_dir = setPositionDirichletBC(this, displ_dir)
@@ -183,11 +270,11 @@ classdef ShakerDefault < models.muscle.AMuscleConfig
         end
         
         function test_ShakerDefault
-            m = models.muscle.Model(ShakerDefault('Frequency',80));
+            m   = models.muscle.Model(ShakerDefault);
             m.simulateAndPlot;
+%             m = models.muscle.Model(ShakerDefault('Frequency',80));
+%             m.simulateAndPlot;
         end
     end
     
 end
-
-
